5dc5d0fddf5866cd034a7392d0498afae5184c05
shreyash talole <shreyastalole2001@gmail.com>
2022-06-02 19:45:47 +0530

    first commit

---
+++ public/client.js
@@ -0,0 +1,62 @@
+const socket = io()
+
+let namee;
+
+let textarea = document.querySelector('#textarea')
+let messageArea = document.querySelector('.message--area')
+
+do{
+    namee = prompt("Please enter your name: ")
+}while(!namee)
+
+textarea.addEventListener('keyup', (e) => {
+
+    if(e.key === 'Enter'){
+        sendMessage(e.target.value)
+    }
+
+})
+
+function sendMessage(message) {
+    let msg = {
+        user: namee,
+        message: message.trim()
+    }
+
+    // Append
+    appendMessage(msg, 'outgoing')
+    textarea.value = ''
+    scrollToBottom()
+    //send to server
+    socket.emit('message', msg)
+  
+}
+
+function appendMessage(msg, type){
+
+    let mainDiv = document.createElement('div')
+    let className = type
+    mainDiv.classList.add(className, 'message')
+
+    let markup = `
+    <h4>${msg.user}</h4>
+    <p>${msg.message}</p>
+    `
+
+    mainDiv.innerHTML = markup
+    messageArea.appendChild(mainDiv)
+
+}
+
+// Receive Message
+socket.on('message', (msg) => {
+    appendMessage(msg, 'incomming')
+    scrollToBottom()
+})
+
+
+function scrollToBottom() {
+    messageArea.scrollTop = messageArea.scrollHeight
+}
+
+
